d38b8c3a0d71c3f1db5399d709cb14c20f35a000
akauntozonghe-cpu <akauntozonghe@gmail.com>
2025-09-21 17:41:15 +0900

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -3,16 +3,19 @@ import { initializeApp, getApps, getApp } from "https://www.gstatic.com/firebase
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "xxxxx",
-  authDomain: "xxxxx.firebaseapp.com",
-  projectId: "xxxxx",
-  storageBucket: "xxxxx.appspot.com",
-  messagingSenderId: "xxxxx",
-  appId: "xxxxx"
+  apiKey: "AIzaSyCqPckkK9FkDkeVrYjoZQA1Y3HuOGuUGwI",
+  authDomain: "inventory-app-312ca.firebaseapp.com",
+  databaseURL: "https://inventory-app-312ca-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "inventory-app-312ca",
+  storageBucket: "inventory-app-312ca.firebasestorage.app",
+  messagingSenderId: "245219344089",
+  appId: "1:245219344089:web:e46105927c302e6a5788c8",
+  measurementId: "G-TRH31MJCE3"
 };
 
-// すでに初期化済みなら再利用
+// ✅ 二重初期化防止
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
